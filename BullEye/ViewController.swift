a5e61651c4dee7ddd4808f2913d8c01b5b8166b5
ciec20151104696 <1559301693@qq.com>
2018-05-11 08:45:43 +0800

    Initial Commit

---
+++ BullEye/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BullEye
+//
+//  Created by s20151104696 on 18/5/11.
+//  Copyright © 2018年 s20151104696. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
