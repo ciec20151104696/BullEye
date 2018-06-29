ed8a2d0dbb03ac4239153b4c82f72b43ec3e7b5e
ciec20151104696 <1559301693@qq.com>
2018-06-29 11:40:59 +0800

    添加游戏规则
    点击屏幕中的叹号会调到游戏规则界面，点击返回游戏后，回到初始界面

---
+++ BullEye/AboutViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AboutViewController.swift
+//  BullEye
+//
+//  Created by s20151104696 on 18/6/29.
+//  Copyright © 2018年 s20151104696. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    @IBAction func close(){
+        dismiss(animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
